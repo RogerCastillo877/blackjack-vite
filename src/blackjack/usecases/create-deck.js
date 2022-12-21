a34c52dfa7bd3679f2c323f9486e88b7c62bf98b
RogerCastillo877 <roger.castillo877@gmail.com>
2022-12-21 15:02:26 -0500

    Improving documentation JSDoc Comments

--- src/blackjack/usecases/create-deck.js
+++ src/blackjack/usecases/create-deck.js
@@ -1,7 +1,15 @@
 import _ from 'underscore';
 
+/**
+ * @param { Array<string> } cardTypes Ex: ['C', 'D']
+ * @param { Array<string> } specialTypes Ex: ['A', 'J']
+ * @returns { Array } returns a new deck of cards
+*/
+
 export const createDeck = (cardTypes, specialTypes) => {
 
+  if (!cardTypes || !cardTypes.length > 0) throw new Error('cardTypes is required and must be an array of strings');
+  if (!specialTypes || !specialTypes.length > 0) throw new Error('specialTypes is required and must be an array of strings');
   let deck = [];
 
   for (let i = 2; i <= 10; i++) {
